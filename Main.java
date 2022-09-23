a0c5a769f421c05967c97ef5657517ff91bbdd7e
GulamMoinuddin <gmoinuddi656@gmail.com>
2022-09-23 16:03:51 +0530

    Linked list Created

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class Main{
+    public static void main(String args[]){
+
+        LinkedList<String> name = new LinkedList<>();
+        name.add("Amit");
+        name.add("Sumit");
+        name.add("Gulam");
+        name.add("Altab");
+
+        System.out.println("Linked list: "+name);
+    }
+}
